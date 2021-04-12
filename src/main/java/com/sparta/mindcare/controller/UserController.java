b5486224cd7cec695d373c2268c225ac5503354f
Jaeseung Choun <54395509+lovelyAlien@users.noreply.github.com>
2021-04-12 15:17:52 +0900

    update User

--- src/main/java/com/sparta/mindcare/controller/UserController.java
+++ src/main/java/com/sparta/mindcare/controller/UserController.java
@@ -1,4 +1,5 @@
 package com.sparta.mindcare.controller;
 
 public class UserController {
+
 }
--- src/main/java/com/sparta/mindcare/model/User.java
+++ src/main/java/com/sparta/mindcare/model/User.java
@@ -1,4 +1,30 @@
 package com.sparta.mindcare.model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class User extends Timestamped{
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+    // ID가 자동으로 생성 및 증가합니다.
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    // 반드시 값을 가지도록 합니다.
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+//    @Column(nullable = false)
+//    private String email;
+
 }
