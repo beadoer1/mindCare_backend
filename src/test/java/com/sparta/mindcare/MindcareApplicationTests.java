f3aec1e8cc7568ca04da38c6730ec4780ec376ba
beadoer1 <beadoer1@gmail.com>
2021-04-12 11:55:35 +0900

    설정파일 추가 후 commit

---
+++ src/test/java/com/sparta/mindcare/MindcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.mindcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
